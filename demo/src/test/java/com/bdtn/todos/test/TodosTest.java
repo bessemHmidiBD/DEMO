8771d07817d9de3c7d3a6df3c9e0ec3304b8abf8
bessem.hmidi <bessem.hmidi.perso@gmail.com>
2016-05-12 23:45:39 -0700

    Signed-off-by: bessem.hmidi <bessem.hmidi.perso@gmail.com>

---
+++ demo/src/test/java/com/bdtn/todos/test/TodosTest.java
@@ -0,0 +1,17 @@
+package com.bdtn.todos.test;
+
+import static org.junit.Assert.*;
+
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+ 
+@RunWith(CdiRunner.class)
+public class TodosTest {  
+
+	@Test 
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
