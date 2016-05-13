05ede020eded7160a9bc331988344bccb7010cab
bessem.hmidi <bessem.hmidi.perso@gmail.com>
2016-05-13 00:14:50 -0700

    Signed-off-by: bessem.hmidi <bessem.hmidi.perso@gmail.com>

--- demo/src/test/java/com/bdtn/todos/test/TodosTest.java
+++ demo/src/test/java/com/bdtn/todos/test/TodosTest.java
@@ -2,16 +2,29 @@ package com.bdtn.todos.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.bdtn.todos.domain.Todos;
+import com.bdtn.todos.service.TodosEndPoint;
  
 @RunWith(CdiRunner.class)
 public class TodosTest {  
 
+	
+	@Inject
+	TodosEndPoint todosEndPoint;
+	
 	@Test 
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAllTodos() {
+		List<Todos> result=todosEndPoint.getAll();
+		
+		assertEquals(2, result.size());
 	}
 
 }
